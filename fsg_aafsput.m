760d8e7fddcf6a37053b30ad96d1620db0ea2b12
joelliew91 <joelliew91@gmail.com>
2015-09-21 10:12:35 +0800

    tut3 done

--- fsg_aafsput.m
+++ fsg_aafsput.m
@@ -1,54 +1,51 @@
 %fsg_aafsput(r,q,s,sigma,t,N,rho)
-function fsg_aafsput1(r,q,s,sigma,t,N,rho)
+function fsg_aafsput(r,q,s,sigma,t,N,rho)
+tic
 dt=t/N;
 u=exp(sigma*sqrt(dt));
 d=1/u;
 disc = exp(-r*dt);
 p = (exp((r-q)*dt)-d)/(u-d);
 dy = rho*sigma*sqrt(dt);
+m=1/rho;
 
-ind = N:-2:-N;
-S = s*u.^ind;
-k=N/rho;
-
-
-S_hist = repmat(s,2^N,1);
-A_floor_hist = repmat(s,2^N,1);
-A_ceil_hist = repmat(s,2^N,1);
-k_floor = zeros(2^N,1);
-k_ceil = zeros(2^N,1);
-A_hist = repmat(s,2^N,1);
-for i=1:1:N
-    UP=repmat(u,2^(N-i),1);
-    DN=repmat(d,2^(N-i),1);
-    combine = [UP;DN];
-    combine = repmat(combine,2^(i-1),1);
-    S_hist = [S_hist S_hist(:,i).*combine];
-    A_hist = [A_hist (A_hist(:,i)*i+S_hist(:,i+1))/(i+1)];
-    AF =  (A_floor_hist(:,i)*i+S_hist(:,i+1))/(i+1);
-    k_floor = [k_floor floor(log(AF/s)/dy)];
-    A_floor = exp(k_floor(:,i+1)*dy)*s;
-    A_floor_hist = [A_floor_hist A_floor];
-    AC = (A_ceil_hist(:,i)*i+S_hist(:,i+1))/(i+1);
-    k_ceil = [k_ceil ceil(log(AC/s)/dy)];
-    A_ceil = exp(k_ceil(:,i+1)*dy)*s;
-    A_ceil_hist = [A_ceil_hist A_ceil];
+for i=0:N
+final = i*m:-1:-i*m;
+final_s = i:-2:-i;
+S{i+1} = s*u.^final_s;
+A{i+1} = s*exp(final*dy);
 end
-
-V_up=max(A_ceil_hist(:,N+1)-S_hist(:,N+1),0);
-V_dn=max(A_floor_hist(:,N+1)-S_hist(:,N+1),0);
-A_floor_hist(:,N+1)
-fac = A_hist(:,N+1) - A_floor_hist(:,N+1);
-fac = fac./(A_ceil_hist(:,N+1)-A_floor_hist(:,N+1));
-V =V_up.*fac+V_dn.*(1-fac)
-
+for i=1:N+1
+    W{i} = max(A{N+1}-S{N+1}(i),0)';
+end
 for i=N:-1:1
-    up_ind=1:2:2^i;
-    dn_ind=2:2:2^i;
-    fac_ind = 1:2^(N-i+1):2^N;
-    V=disc*(p*V(up_ind)+(1-p)*V(dn_ind))
-    fac = A_hist(fac_ind,i+1) - A_floor_hist(fac_ind,i+1);
-    fac = fac./(A_ceil_hist(fac_ind,i+1)-A_floor_hist(fac_ind,i+1)) 
-end
+    for j=1:1:i
+        A_u = (S{i}(j)*u+i*A{i})/(i+1);
+        A_d = (S{i}(j)*d+i*A{i})/(i+1);
+        id_A_u_floor = floor(log(A_u/s)/dy);
+        id_A_d_floor = floor(log(A_d/s)/dy);
+        
+        V_up_l = W{j}(m*i-id_A_u_floor+1);
+        V_up_h = W{j}(m*i-id_A_u_floor);     
+        x_up_h = A{i+1}(m*i-id_A_u_floor);
+        x_up_l = A{i+1}(m*i-id_A_u_floor+1);
+        fac = x_up_h - x_up_l;
+        fac = ((A_u-x_up_l)./fac)';
+        V_up = fac.*V_up_h+(1-fac).*V_up_l;
 
+        V_dn_l = W{j+1}(m*i-id_A_d_floor+1);
+        V_dn_h = W{j+1}(m*i-id_A_d_floor);
+        x_dn_l = A{i+1}(m*i-id_A_d_floor+1);
+        x_dn_h = A{i+1}(m*i-id_A_d_floor);
+        fac = x_dn_h - x_dn_l;
+        fac = ((A_d-x_dn_l)./fac)';
+        V_dn= fac.*V_dn_h+(1-fac).*V_dn_l;
+        
+        F{j}=disc*(p*V_up+(1-p)*V_dn);
+        
+    end
+    W = F;
+end
+W{1}
+toc
 return
